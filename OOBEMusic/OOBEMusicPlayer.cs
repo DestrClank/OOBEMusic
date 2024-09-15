62b95a398be458fcae72a064886a221f4152514a
DestrClank <dylanvassalo73@gmail.com>
2024-09-15 15:21:21 +0200

    modification des messages logs

--- OOBEMusic/OOBEMusicPlayer.cs
+++ OOBEMusic/OOBEMusicPlayer.cs
@@ -1,18 +1,13 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.ComponentModel;
 using System.Data;
 using System.Diagnostics;
 using System.Linq;
 using System.ServiceProcess;
 using System.Media;
-using System.Text;
 using System.Threading;
 using System.Threading.Tasks;
-using NAudio.Wave;
 using Microsoft.Extensions.Logging;
 using Microsoft.Extensions.Hosting;
-using Microsoft.Win32;
 
 namespace OOBEMusic
 {
@@ -27,12 +22,12 @@ namespace OOBEMusic
         static string SuperVerboseLogsKeyName = "EnableSuperVerboseLogs";
         static int SuperVerboseLogs = 0;
 
-        ServiceBase service =  new ServiceBase();
+        ServiceBase service = new ServiceBase();
 
         public OOBEMusicPlayer(ILogger<OOBEMusicPlayer> logger) // Injectez le logger dans le constructeur
         {
             this.ServiceName = servicename;
-            
+
             InitializeComponent();
             _logger = logger;
         }
@@ -51,11 +46,11 @@ namespace OOBEMusic
 
         protected override void OnStart(string[] args)
         {
-            int WWAHostState = RegHelper.CheckActivationState(WWAHostKeyName,_logger);
+            int WWAHostState = RegHelper.CheckActivationState(WWAHostKeyName, _logger);
             int FirstLogonState = RegHelper.CheckActivationState(FirstLogonAnimKeyName, _logger);
             SuperVerboseLogs = RegHelper.CheckActivationState(SuperVerboseLogsKeyName, _logger, 0);
 
-            if (WWAHostState != 1 && FirstLogonState != 1) 
+            if (WWAHostState != 1 && FirstLogonState != 1)
             {
                 _logger.LogInformation($"Les deux valeurs {WWAHostKeyName} et {FirstLogonAnimKeyName} ne sont pas réglés sur 1. Le programme va s'arrêter.");
                 service.Stop();
@@ -66,15 +61,15 @@ namespace OOBEMusic
             }
             string appPath = AppDomain.CurrentDomain.BaseDirectory;
             string musicPath = appPath + "music.wav";
-            RegHelper.SaveKey(musicPath,_logger);      
+            RegHelper.SaveKey(musicPath, _logger);
             if (SuperVerboseLogs == 1)
             {
-                _logger.LogInformation("Ta mere la pute la salope. \nEn language sérieux : Le service OOBEMusic.exe a été lancé avec succès.");
+                _logger.LogInformation("uiiiii ca marcheeee \nEn language sérieux : Le service OOBEMusic.exe a été lancé avec succès.");
             } else
             {
                 _logger.LogInformation("Le service OOBEMusic.exe a été lancé avec succès.");
             }
-            
+
         }
 
         static void HookIntoWWA(ILogger logger)
@@ -88,7 +83,7 @@ namespace OOBEMusic
 
             while (true)
             {
-                if (Checkifprocessexists(WWAHostActivate, FirstLogonAnimActivate) && !musicPlaying)
+                if (Checkifprocessexists(WWAHostActivate, FirstLogonAnimActivate, logger) && !musicPlaying)
                 {
                     PlaySound(player, logger, true, out musicPlaying, out musicPath);
 
@@ -99,18 +94,18 @@ namespace OOBEMusic
                     {
                         logger.LogInformation($"Le processus WWAHost.exe a été détécté et le programme lance la musique.\nEmplacement du fichier : {musicPath}");
                     }
-                    
 
-                } else if (!Checkifprocessexists(WWAHostActivate, FirstLogonAnimActivate) && musicPlaying)
+
+                } else if (!Checkifprocessexists(WWAHostActivate, FirstLogonAnimActivate, logger) && musicPlaying)
                 {
                     if (SuperVerboseLogs == 1)
                     {
-                        logger.LogInformation("La musique s'est arrêté, normalement WALLAH c'est arrêté.\nEn language sérieux : Le processus WWAHost.exe a été fermé, la musique va s'arrêter.");
+                        logger.LogInformation("La musique s'est arrêté, normalement c'est arrêté.\nEn language sérieux : Le processus WWAHost.exe a été fermé, la musique va s'arrêter.");
                     } else
                     {
                         logger.LogInformation("Le processus WWAHost.exe a été fermé, la musique va s'arrêter.");
                     }
-                    
+
                     musicPlaying = false;
                     player.Stop();
                 }
@@ -135,15 +130,15 @@ namespace OOBEMusic
             {
                 if (SuperVerboseLogs == 1)
                 {
-                    logger.LogError($"TA GRAND MERE LA REINE DES PUTES TU T4APPELLES INES OU QUOIII TA DARONNE SUCE DES BITES : {ex.Message} \nEn language sérieux : Une erreur est survenue dans l'application empêchant la lecture de la musique, vérifiez que le fichier est bien un fichier .wav et que l'emplacement indiqué dans le registre Windows à la clé HKEY_LOCAL_MACHINE\\SOFTWARE\\WOW6432Node\\DestrClank\\OOBEMusic\\MusicFile indique un emplacement de fichier valide à cette adresse et que le fichier existe et est accessible. \nCode d'erreur : {ex.Message}");
+                    logger.LogError($"naaaahhhh ca marche pluuu: {ex.Message} \nEn language sérieux : Une erreur est survenue dans l'application empêchant la lecture de la musique, vérifiez que le fichier est bien un fichier .wav et que l'emplacement indiqué dans le registre Windows à la clé HKEY_LOCAL_MACHINE\\SOFTWARE\\WOW6432Node\\DestrClank\\OOBEMusic\\MusicFile indique un emplacement de fichier valide à cette adresse et que le fichier existe et est accessible. \nCode d'erreur : {ex.Message}");
                 } else
                 {
                     logger.LogError($"Une erreur est survenue dans l'application empêchant la lecture de la musique, vérifiez que le fichier est bien un fichier .wav et que l'emplacement indiqué dans le registre Windows à la clé HKEY_LOCAL_MACHINE\\SOFTWARE\\WOW6432Node\\DestrClank\\OOBEMusic\\MusicFile indique un emplacement de fichier valide à cette adresse et que le fichier existe et est accessible. \nCode d'erreur : {ex.Message}");
-                }     
+                }
             }
         }
 
-        static bool Checkifprocessexists(int WWAHostSetting, int FirstLogonSetting)
+        static bool Checkifprocessexists(int WWAHostSetting, int FirstLogonSetting, ILogger logger)
         {
             string wwahost = "WWAHost";
             string firstbootanim = "FirstLogonAnim";
@@ -155,23 +150,49 @@ namespace OOBEMusic
 
             if (wwahostlist.Length > 0 && WWAHostSetting == 1)
             {
-                exists = true;
+
+                bool interrupted = CheckProcesses(wwahostlist);
+
+                if (!interrupted)
+                {
+                    exists = true;
+                }
             }
 
             if (firstlogonlist.Length > 0 && FirstLogonSetting == 1)
             {
-                exists = true;
+
+                bool interrupted = CheckProcesses(firstlogonlist);
+
+                if (!interrupted)
+                {
+                    exists = true;
+                }
+
             }
-
             return exists;
-
         }
+
+        static bool CheckProcesses(Process[] processes)
+        {
+            foreach (Process process in processes)
+            {
+                bool isSuspended = process.Threads.Cast<ProcessThread>()
+                    .Any(thread => thread.ThreadState == System.Diagnostics.ThreadState.Wait && thread.WaitReason == ThreadWaitReason.Suspended);
+                if (!isSuspended)
+                {
+                    return false; // Si un processus n'est pas suspendu, retourne false
+                } //
+            }
+            return true; // Si tous les processus sont suspendus, retourne true
+        }
+
 
         protected override void OnStop()
         {
             if (SuperVerboseLogs == 1)
             {
-                _logger.LogWarning("POURQUOI TU VEUX ME TUERR WSHH T PAS GENTIL FRERO §§§§ :((((((( JE VAIS CHIALLERRR JE VEUX PAS MOURRRIRRRRRR NOOOOOOOOOOOOOOOOOOOOOOONNNNNNNNNNNNNNNNNNNNNNNNNNNN \nEn language sérieux : Le processus a recu une demande d'arrêt du système. Le processus va s'arrêter.");
+                _logger.LogWarning("JE VEUX PAS MOURRRIRRRRRR NOOOOOOOOOOOOOOOOOOOOOOONNNNNNNNNNNNNNNNNNNNNNNNNNNN \nEn language sérieux : Le processus a recu une demande d'arrêt du système. Le processus va s'arrêter.");
             } else
             {
                 _logger.LogWarning("Le processus a recu une demande d'arrêt du système. Le processus va s'arrêter.");
