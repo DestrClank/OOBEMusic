a78788ae179d51172e8441f772bcea19692ece32
DestrClank <dylanvassalo73@gmail.com>
2024-09-15 15:21:21 +0200

    modification des messages logs

--- OOBEMusic/Program.cs
+++ OOBEMusic/Program.cs
@@ -1,12 +1,5 @@
-﻿using Microsoft.Extensions.DependencyInjection;
-using Microsoft.Extensions.Hosting;
-using Microsoft.Extensions.Logging;
-using System;
-using System.Collections.Generic;
-using System.Linq;
+﻿using Microsoft.Extensions.Logging;
 using System.ServiceProcess;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace OOBEMusic
 {
--- OOBEMusic/Registry.cs
+++ OOBEMusic/Registry.cs
@@ -1,13 +1,6 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-using System.IO;
-using Microsoft.Extensions.Configuration;
 using Microsoft.Win32;
 using Microsoft.Extensions.Logging;
-using System.Xml.Linq;
 
 namespace OOBEMusic
 {
