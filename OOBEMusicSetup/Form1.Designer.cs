642cac34671232f19a59e5aace268b5cd342a612
DestrClank <dylanvassalo73@gmail.com>
2025-05-03 21:45:13 +0200

    a test app is added to test

--- OOBEMusicSetup/Form1.Designer.cs
+++ OOBEMusicSetup/Form1.Designer.cs
@@ -42,13 +42,14 @@
             this.threadTimeoutNumber = new System.Windows.Forms.NumericUpDown();
             this.label2 = new System.Windows.Forms.Label();
             this.groupBox4 = new System.Windows.Forms.GroupBox();
+            this.button3 = new System.Windows.Forms.Button();
             this.button2 = new System.Windows.Forms.Button();
             this.button1 = new System.Windows.Forms.Button();
             this.serviceStatusLabel = new System.Windows.Forms.Label();
             this.disableServiceButton = new System.Windows.Forms.Button();
             this.stopServiceButton = new System.Windows.Forms.Button();
             this.restartServiceButton = new System.Windows.Forms.Button();
-            this.button3 = new System.Windows.Forms.Button();
+            this.button4 = new System.Windows.Forms.Button();
             this.groupBox1.SuspendLayout();
             this.groupBox2.SuspendLayout();
             this.groupBox3.SuspendLayout();
@@ -219,6 +220,7 @@
             // 
             this.groupBox4.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left) 
             | System.Windows.Forms.AnchorStyles.Right)));
+            this.groupBox4.Controls.Add(this.button4);
             this.groupBox4.Controls.Add(this.button3);
             this.groupBox4.Controls.Add(this.button2);
             this.groupBox4.Controls.Add(this.button1);
@@ -228,10 +230,20 @@
             this.groupBox4.Controls.Add(this.restartServiceButton);
             this.groupBox4.Location = new System.Drawing.Point(13, 277);
             this.groupBox4.Name = "groupBox4";
-            this.groupBox4.Size = new System.Drawing.Size(637, 100);
+            this.groupBox4.Size = new System.Drawing.Size(637, 75);
             this.groupBox4.TabIndex = 3;
             this.groupBox4.TabStop = false;
             this.groupBox4.Text = "Service Control";
+            // 
+            // button3
+            // 
+            this.button3.Location = new System.Drawing.Point(494, 19);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(117, 23);
+            this.button3.TabIndex = 6;
+            this.button3.Text = "Open Event Viewer";
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
             // 
             // button2
             // 
@@ -296,15 +308,15 @@
             this.restartServiceButton.UseVisualStyleBackColor = true;
             this.restartServiceButton.Click += new System.EventHandler(this.restartServiceButton_Click);
             // 
-            // button3
+            // button4
             // 
-            this.button3.Location = new System.Drawing.Point(494, 19);
-            this.button3.Name = "button3";
-            this.button3.Size = new System.Drawing.Size(117, 23);
-            this.button3.TabIndex = 6;
-            this.button3.Text = "Open Event Viewer";
-            this.button3.UseVisualStyleBackColor = true;
-            this.button3.Click += new System.EventHandler(this.button3_Click);
+            this.button4.Location = new System.Drawing.Point(494, 46);
+            this.button4.Name = "button4";
+            this.button4.Size = new System.Drawing.Size(117, 23);
+            this.button4.TabIndex = 7;
+            this.button4.Text = "Test Application";
+            this.button4.UseVisualStyleBackColor = true;
+            this.button4.Click += new System.EventHandler(this.button4_Click);
             // 
             // Form1
             // 
@@ -354,6 +366,7 @@
         private System.Windows.Forms.Button button1;
         private System.Windows.Forms.Button button2;
         private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.Button button4;
     }
 }
 
--- OOBEMusicSetup/Form1.cs
+++ OOBEMusicSetup/Form1.cs
@@ -4,6 +4,7 @@ using System.ComponentModel;
 using System.Data;
 using System.Diagnostics;
 using System.Drawing;
+using System.IO;
 using System.Linq;
 using System.ServiceProcess;
 using System.Text;
@@ -358,6 +359,14 @@ namespace OOBEMusicSetup
         {
             //open event viewer
             Process.Start("eventvwr.msc");
+        }
+
+        private void button4_Click(object sender, EventArgs e)
+        {
+            if (File.Exists("WWAHost.exe"))
+            {
+                Process.Start("WWAHost.exe");
+            } else { MessageBox.Show("WWAHost.exe not found in the current directory.", "Error", MessageBoxButtons.OK, MessageBoxIcon.Error); }
         }
     }
 }
